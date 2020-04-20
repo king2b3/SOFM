308ee83b5aecbca456188065d0e4cc40290f1c51
Bayley King Windows <king2b3@mail.uc.edu>
2020-04-20 16:45:57 -0400

    plotting changes
    init of Final Paper Folder
    Resize and Plotting updates

--- params.py
+++ params.py
@@ -1,14 +1,14 @@
 # Paramater Definitions
 
-max_epochs = 1000
+max_epochs = 100
 no = .01
 tau = max_epochs/2
 tauN = max_epochs/5
 sigmaP = 100
 #batch_size = 50                # uncomment for batch training
 trainBool = True
-layers = [10000,100]
-pathExt = "SavedWeights/"
+layers = [1000000,100]
+pathExt = "SavedWeights/FromBayley/1000k/savedWeights/"
 SimParm = pathExt+"BestParam.txt"
 Metrics = pathExt+"metrics.p"
 MapPickle = pathExt+"map.p"
